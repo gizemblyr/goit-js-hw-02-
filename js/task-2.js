b2f99f96586793d6a88e10bff4f372ff9d5ecd58
gizem <gizembolayir@gmail.com>
2025-06-28 17:42:15 +0300

    js hw 2

---
+++ js/task-2.js
@@ -0,0 +1,22 @@
+
+function formatMessage(message, maxLength){
+  if(message.length<=maxLength){
+    return message;
+  } 
+  else{ 
+    let dize='';
+    for(let i=0; i<=maxLength; i++){
+      dize+=message[i];     
+    }
+     message = `${dize}...`;
+    return message;
+  }
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
